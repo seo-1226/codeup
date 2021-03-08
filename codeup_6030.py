1643931eb0a32f2bd88e030a262e398756db8661
seo-h2 <seo-1226@users.noreply.github.com>
2021-03-08 22:22:58 +0900

    Create codeup_6030.py

---
+++ codeup_6030.py
@@ -0,0 +1,118 @@
+# # ============6001
+# print("Hello")
+
+# # ============6002
+# print("Hello World")
+
+# #==========6003
+# print("Hello\nWorld")
+
+# #==========6004
+# print("'Hello'")
+
+# #=========6005
+# print("\"Hello World\"")
+
+# #=========6006
+# print("\"!@#$%^&*()'")
+
+# #============6007
+# print("\"C:\Download\'hello'.py\"")
+
+# #=========6008
+# print("print(\"Hello\\nWorld\")")
+
+# #==========6009
+# a= input()
+# print(a)
+
+# #========6010
+# a= int(input())
+# print(a)
+
+# #========6011
+# f= float(input())
+# print(f)
+
+# #========6012
+# a= int(input())
+# b=int(input())
+# print(a,'\n',b,sep='')
+
+# #========6013
+# a= input()
+# b= input()
+# print(b)
+# print(a)
+
+# #=========6014
+# a= input()
+# for i in range(3):
+#     print(a)
+
+# #==========6015
+# lst= input().split()
+# print(lst[0])
+# print(lst[1])
+
+# #=========6016
+# a,b= input().split()
+# print(b, a)
+
+# #=======6017
+# a= input()
+# print(a,a,a)
+
+# #========6018
+# a,b= input().split(":")
+# print(a,b,sep=":")
+
+# #==========6019
+# y,m,d= input().split(".")
+# print(d,m,y,sep="-")
+
+#==========6020
+# lst= input().split("-")
+# print(lst[0]+lst[1])
+
+# #==========6021
+# a=input()
+# for i in a:
+#     print(i)
+
+# #=========6022
+# a=input()
+# print(a[0:2],a[2:4], a[4:])
+
+# #=========6023
+# a= input().split(":")
+# print(a[1])
+
+# #==========6024
+# a= input().split()
+# print(a[0]+a[1])
+
+# #========6025
+# a,b= input().split()
+# print(int(a)+int(b))
+
+# #=========6026
+# a= float(input())
+# b= float(input())
+# print(a+b)
+
+# #=========6027
+# a= int(input())
+# print('%x'%a)
+
+# #===========6028
+# a= int(input())
+# print('%X'%a)
+
+# #========6029
+# a= int(input(),16)
+# print('%o'%a)
+
+# #==========6030
+# a= ord(input())
+# print(a)
