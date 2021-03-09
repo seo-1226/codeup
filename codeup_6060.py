d2e706323b55442434cd2580cbdd73fe7f606ee1
seo-h2 <seo-1226@users.noreply.github.com>
2021-03-10 01:21:35 +0900

    Create codeup_6060.py

---
+++ codeup_6060.py
@@ -0,0 +1,120 @@
+# #===6031
+# a= int(input())
+# print(chr(a))
+
+# #===6032
+# a= int(input())
+# print(-a)
+
+# #===6033
+# a= ord(input())
+# print(chr(a+1))
+
+# #===6034
+# a,b= input().split()
+# print(int(a)-int(b))
+
+# #===6035
+# a,b= input().split()
+# print(float(a)*float(b))
+
+# #===6036
+# a= input()
+# print(a*3)
+
+# #===6037
+# n=int(input())
+# a= input()
+# print(a*n)
+
+# #===6038
+# a,b= input().split()
+# print(int(a)**int(b))
+
+# #===6039
+# a,b= input().split()
+# print(float(a)**float(b))
+
+# #===6040
+# a,b= map(int,input().split())
+# print(a//b)
+
+# #===6041
+# a,b= map(int,input().split())
+# print(a%b)
+
+# #===6042
+# a= input()
+# print(round(float(a),2))
+
+# #===6043
+# a,b= map(float, input().split())
+# print('%.3f'%(a/b))
+
+# #===6044
+# a,b= map(int, input().split())
+# print(a+b, a-b, a*b, a//b,a%b,round(a/b,2), sep='\n')
+
+# #===6045
+# a,b,c= map(int,input().split())
+# print(a+b+c, round((a+b+c)/3,2))
+
+# #===6046
+# a= int(input())
+# print(a<<1)
+
+#===6047
+# a,b= map(int,input().split())
+# print(a<<b)
+
+# #===6048
+# a,b= map(int,input().split())
+# print(a<b)
+
+# #===6049
+# a,b= map(int,input().split())
+# print(a==b)
+
+# #===6050
+# a,b= map(int,input().split())
+# print(a<=b)
+
+# #===6051
+# a,b= map(int,input().split())
+# print(a!=b)
+
+# #===6052
+# a= input()
+# print(bool(int(a)))
+
+# #===6053
+# a= input()
+# print(not bool(int(a)))
+
+# #===6054
+# a,b= map(int,input().split())
+# print(bool(a)&bool(b))
+
+# #===6055
+# a,b= map(int,input().split())
+# print(bool(a)|bool(b))
+
+# #===6056
+# a,b= map(int,input().split())
+# print(bool(a)!=bool(b))
+
+# #===6057
+# a,b= map(int,input().split())
+# print(bool(a)==bool(b))
+
+# #===6058
+# a,b= map(int,input().split())
+# print(not bool(a) and not bool(b))
+
+# #===6059
+# a= int(input())
+# print(~a)
+
+# #===6060
+# a,b= map(int,input().split())
+# print(a&b)
